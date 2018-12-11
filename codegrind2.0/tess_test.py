a0fa0b6e3ad3b5579365d878a22b3d5a318b5015
Siddhartha Khanooja <siddhartha.khanooja@gmail.com>
2018-12-11 18:34:46 +0530

    added functions for optional smoothing using morphology

--- codegrind2.0/tess_test.py
+++ codegrind2.0/tess_test.py
@@ -9,6 +9,9 @@ import time
 import argparse
 from statistics import mode
 from pdf2image import convert_from_path
+import imutils
+import tempfile
+from PIL import Image
 
 output_dir = "D:\\test"
 
@@ -32,7 +35,7 @@ def get_string(img_path, method):
     file_name = os.path.basename(img_path).split('.')[0]
     file_name = file_name.split()[0]
     if 'pdf' in img_path:
-    	pages = convert_from_path(img_path, 500)
+    	pages = convert_from_path(img_path, 300)
     	page = pages[0]
     	page.save('test.png', 'PNG')
     	img = cv2.imread('test.png')
@@ -42,17 +45,20 @@ def get_string(img_path, method):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     # Rescale the image, if needed.
+    #img = imutils.rotate_bound(img, 270)
     img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
     # Convert to gray
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Apply dilation and erosion to remove some noise
-    #kernel = np.ones((1, 1), np.uint8)
-    #img = cv2.dilate(img, kernel, iterations=1)
-    #img = cv2.erode(img, kernel, iterations=1)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
 
     # Apply blur to smooth out the edges
-    #img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+
+    #img = remove_noise_and_smooth(img)
 
     # Apply threshold to get image with only b&w (binarization)
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
@@ -62,7 +68,7 @@ def get_string(img_path, method):
         #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
         #img = clahe.apply(img)
         img = cv2.equalizeHist(img)
-        method = 6
+        method = 7
     
     img = apply_threshold(img, method)
     # Save the filtered image in the output directory
@@ -73,12 +79,54 @@ def get_string(img_path, method):
     result = pytesseract.image_to_string(img, lang="eng")
     return result
 
+	
+def remove_noise_and_smooth(img):
+   filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 41, 3)
+   kernel = np.ones((1, 1), np.uint8)
+   opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+   closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+   img = image_smoothening(img)
+   img = cv2.bitwise_or(img, closing)
+   return img
+
+def image_smoothening(img):
+   ret1, th1 = cv2.threshold(img, BINARY_THREHOLD, 255, cv2.THRESH_BINARY)
+   ret2, th2 = cv2.threshold(th1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+   blur = cv2.GaussianBlur(th2, (1, 1), 0)
+   ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+   return th3
+
+def rotate_bound(image, angle):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+ 
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+ 
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+ 
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+ 
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH))
+
 if __name__ == "__main__":
 	#img_path = 'C:\\Pune_Hyderabad.ETicket-1.png'
 	#img_path = 'C:\\Pune_Hyderabad.ETicket.pdf'
-	img_path = 'C:\\images\\boarding_pass.jpg'
-	final = get_string(img_path, 8)
-	text_file = open("D:\\test\\Output.txt", "w")
-	text_file.write(final)
-	text_file.close() 
+	img_path = 'C:\\images\\img2.png'
+	for i in range(1,8):
+		final = get_string(img_path, i)
+		text_file = open("D:\\test\\Output{x}.txt".format(x=i), "w")
+		text_file.write(final)
+		text_file.close() 
 	print("Successful")
